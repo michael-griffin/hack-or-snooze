48f6836d6ddb2a89ac96cc3587b43a00167b26c3
Michael <michaellngriffin@gmail.com>
2023-08-18 16:04:00 -0400

    refactor of handleStarClick, added filled stars

--- js/stories.js
+++ js/stories.js
@@ -22,11 +22,20 @@ async function getAndShowStoriesOnStart() {
 function generateStoryMarkup(story) {
   // console.debug("generateStoryMarkup", story);
 
+  //check if story is in favorites, then add star
+  const unfilledStar = '<i class="favorite-star bi bi-star"></i>';
+  const filledStar = '<i class="favorite-star bi bi-star-fill"></i>';
+
+  let favIds = currentUser.favorites.map(favorite => favorite.storyId);
+  let isFav = favIds.includes(story.storyId);
+
   const hostName = story.getHostName();
   //console.log("hostname, ", hostName)
+  //<i class="favorite-star bi bi-star"></i>
   return $(`
       <li id="${story.storyId}">
-        <i class="favorite-star bi bi-star"></i>
+        ${isFav ? filledStar : unfilledStar}
+
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
@@ -37,14 +46,16 @@ function generateStoryMarkup(story) {
     `);
 }
 
-function generateFavoriteMarkup(story) {
+//refactor:
+//get rid of generateFavoriteMarkup
+/* function generateFavoriteMarkup(story) {
   // console.debug("generateStoryMarkup", story);
 
   const hostName = story.getHostName();
   //console.log("hostname, ", hostName)
   return $(`
       <li id="${story.storyId}">
-        <i class="favorite-star bi bi-star"></i>
+        <i class="favorite-star bi bi-star-fill"></i>
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
@@ -53,7 +64,7 @@ function generateFavoriteMarkup(story) {
         <small class="story-user">posted by ${story.username}</small>
       </li>
     `);
-}
+} */
 
 /** Gets list of stories from server, generates their HTML, and puts on page. */
 
@@ -72,16 +83,16 @@ function putStoriesOnPage() {
 }
 
 /** Get list of favorites, generate HTML, and put on page. */
-function putFavoritesOnPage(){
+function putFavoritesOnPage() {
   console.debug("putFavoritesOnPage");
   $favoritesList.empty();
 
-  if (currentUser.favorites.length === 0){
+  if (currentUser.favorites.length === 0) {
     $('#favorites-default-message').show();
   } else {
     $('#favorites-default-message').hide();
-    for (let favorite of currentUser.favorites){
-      const $favorite = generateFavoriteMarkup(favorite);
+    for (let favorite of currentUser.favorites) {
+      const $favorite = generateStoryMarkup(favorite);
       $favoritesList.append($favorite);
     }
   }
@@ -93,7 +104,7 @@ function putFavoritesOnPage(){
  * and puts story on page.
  */
 
-async function submitNewStory(e){
+async function submitNewStory(e) {
   console.debug("submitNewStory");
 
   e.preventDefault();
@@ -101,7 +112,7 @@ async function submitNewStory(e){
   let title = $("#title-input").val();
   let url = $("#url-input").val();
 
-  const formInputs = {author, title, url};
+  const formInputs = { author, title, url };
   //console.log(currentUser, " is currentUser", formInputs, " formInputs")
   let storyToAdd = await storyList.addStory(currentUser, formInputs);
 
@@ -115,40 +126,46 @@ async function submitNewStory(e){
   $allStoriesList.prepend($story);
   $newStoryForm.hide();
 }
- $("#new-story-submit").on("click", submitNewStory);
+$("#new-story-submit").on("click", submitNewStory);
 
 
 
- async function handleStarClick(evt){
-  console.log("got into handleStarCLick")
+async function handleStarClick(evt) {
   let $closest = $(evt.target).closest('li');
   let $id = $closest.attr("id");
-  console.log('id is: ', $id);
+  console.log('clicked id is: ', $id);
 
   const currentStory = await Story.retrieveStory($id);
   console.log('currentStory', currentStory);
-  //still need to check if currentStory is in user.favorites
 
-  //TODO: check if this logic works!
+  toggleFavorites(currentStory);
 
+  //TODO: we still need to update page on favoritesList
+  //putFavoritesOnPage();
+}
+
+/**
+ * receives a story Id, then checks user's favorites for a possible match
+ * If already a favorite, removes favorite and updates database
+ * Otherwise, adds favorite to currentUser
+ * @param {*} id
+ */
+async function toggleFavorites(story){
   let favIds = currentUser.favorites.map(favorite => favorite.storyId);
-  let alreadyFav = favIds.includes($id);
+  let isFav = favIds.includes(story.id);
 
-  if (!alreadyFav){
-    console.log('attempting to add current story as favorite');
-    await currentUser.addFavorite(currentStory);
+  console.log('isFav is: ', isFav, 'toggling favorite');
+  if (!isFav) {
+    await currentUser.addFavorite(story);
   } else {
-    console.log('attempting to remove current story as favorite');
-    //TODO: potentially update favoritesList with closest here---
-    await currentUser.removeFavorite(currentStory);
+    await currentUser.removeFavorite(story);
   }
   console.log(currentUser.favorites);
+}
 
-/*   let story = storyList.stories[0];   // grab first story on list
-  currentUser.addFavorite(story);
-  console.log(currentUser.favorites); */
-  putFavoritesOnPage();
- }
- //Add event listener to parent element of star
+// let story = storyList.stories[0];   // grab first story on list
+// currentUser.addFavorite(story);
+// console.log(currentUser.favorites);
+//Add event listener to parent element of star
 $favoritesList.on('click', '.favorite-star', handleStarClick);
 $allStoriesList.on('click', '.favorite-star', handleStarClick);
